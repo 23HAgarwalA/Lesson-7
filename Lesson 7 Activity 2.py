2185974623a5137fca9953a1733b4016391efc05
23HAgarwalA <23HAgarwalA@qerdp.co.uk>
2026-02-01 15:41:37 +0000

    initial commit

---
+++ Lesson 7 Activity 2.py
@@ -0,0 +1,16 @@
+a = 123
+b = -989
+print ("a >> 2 =", a >> 2)
+print ("b >> 4=", b >> 4)
+c = 321
+d = -809
+print ("c << 1=", c << 1)
+print ("d << 3=", d << 3)
+
+e = 10
+f = 4
+print (e & f)
+print (~e)
+print (e | f)
+print (e << f)
+print (e >> f)
